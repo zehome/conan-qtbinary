078686c97b3fa34ab1a2e294c2f3200eff37e6f0
Laurent Coustet <laurent.coustet@clarisys.fr>
2018-12-21 10:54:32 +0100

    Update for conan-qt and qt 5.12

--- conanfile.py
+++ conanfile.py
@@ -6,9 +6,22 @@ import os
 import shutil
 
 
+qtmodule_options = (
+    'qt3d', 'qtactiveqt',
+    'qtandroidextras', 'qtbase', 'qtcanvas3d', 'qtcharts', 'qtconnectivity', 'qtdatavis3d',
+    'qtdeclarative', 'qtdoc', 'qtgamepad', 'qtgraphicaleffects', 'qtimageformats',
+    'qtlocation', 'qtmacextras', 'qtmultimedia', 'qtnetworkauth', 'qtpurchasing',
+    'qtqa', 'qtquickcontrols', 'qtquickcontrols2', 'qtremoteobjects', 'qtrepotools',
+    'qtscript', 'qtscxml', 'qtsensors', 'qtserialbus', 'qtserialport', 'qtspeech',
+    'qtsvg', 'qttools', 'qttranslations', 'qtvirtualkeyboard', 'qtwayland', 'qtwebchannel',
+    'qtwebengine', 'qtwebglplugin', 'qtwebsockets', 'qtwebview', 'qtwinextras', 'qtx11extras',
+    'qtxmlpatterns',
+)
+
+
 class QtBinaryConan(ConanFile):
-    name = "Qt"
-    version = "5.11.2"
+    name = "qt"
+    version = "5.12.0"
     description = "Qt binary distributed by Qt company"
     topics = "conan", "qt"
     url = "https://github.com/zehome/conan-qtbinary"
@@ -17,7 +30,34 @@ class QtBinaryConan(ConanFile):
     license = "GPL-3.0-only"
     generators = "txt"
     settings = "os", "compiler", "build_type", "arch"
+    build_requires = (
+        "OpenSSL/1.0.2q@conan/stable",
+    )
 
+    default_options = (
+        "OpenSSL:shared=True",
+    )
+    keep_imports = True
+
+    options = dict({
+        "shared": [True, False],
+        "commercial": [True, False],
+        "opengl": ["no", "es2", "desktop", "dynamic"],
+        "openssl": [True, False],
+        "GUI": [True, False],
+        "widgets": [True, False],
+        "config": "ANY",
+    }, **{opt: [True, False] for opt in qtmodule_options})
+    default_options = dict({
+        "shared": True,
+        "commercial": False,
+        "opengl": "desktop",
+        "openssl": False,
+        "GUI": True,
+        "widgets": True,
+        "config": None,
+    }, **{opt: False for opt in qtmodule_options}
+    )
     def package(self):
         QTSRC = self.env["QT_DIR"]
         for d in ("bin", "include", "lib", "libexec",
@@ -28,3 +68,6 @@ class QtBinaryConan(ConanFile):
 
     def package_info(self):
         self.env_info.path.append(os.path.join(self.package_folder, "bin"))
+
+    def imports(self):
+        self.copy("*", src="bin", dst="bin", root_package="OpenSSL")
